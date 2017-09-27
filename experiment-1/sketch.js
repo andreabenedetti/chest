1db0a2fcb96ab30a8e9271d68864bb280ad9d753
Andrea Benedetti <benedetti.a93@gmail.com>
2017-09-27 12:29:32 +0200

    init
    initialize

---
+++ experiment-1/sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+  createCanvas(800,800);
+}
+
+function draw() {
+  noCursor();
+  background(0);
+  strokeWeight(0);
+  fill(255,65,89);
+  rect(0, 0, mouseX, height);
+  
+  //TEXT
+  if(mouseX > width/2) {
+    s = "Where I receive love from where I give love from";
+    fill(0);
+    text(s, width/2-75, height/2-50, 150, 100);
+  }
+  else {
+    s = "My healed chest wound transformed into a gate";
+    fill(255,65,89);
+    text(s, width/2-75, height/2-50, 150, 100)
+  }
+
+}
